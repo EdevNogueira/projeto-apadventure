43a1d511d75d787067cc533d8d123b42b8af62a0
Edilson Nogueira <edilsonnsf2005@gmail.com>
2024-07-15 12:36:39 -0300

    many updates and more headache

--- escript/script.js
+++ escript/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function(){
     const cabecalho = document.querySelector('header');
     const posicaoScroll = window.scrollY || document.documentElement.scrollTop;
-    if (posicaoScroll > 10) {
+    if (posicaoScroll > 50) {
         cabecalho.classList.add('rolagem');
     } else {
         cabecalho.classList.remove('rolagem');
