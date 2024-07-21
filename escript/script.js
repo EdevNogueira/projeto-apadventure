180b05ef152777995dd9fd98181ae34f700d163a
Edilson Nogueira <edilsonnsf2005@gmail.com>
2024-07-21 16:30:15 -0300

    updates and updates section 2

--- escript/script.js
+++ escript/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function(){
+document.addEventListener('scroll', function(){
     const cabecalho = document.querySelector('header');
     const posicaoScroll = window.scrollY || document.documentElement.scrollTop;
     if (posicaoScroll > 50) {
@@ -6,4 +6,27 @@ window.addEventListener('scroll', function(){
     } else {
         cabecalho.classList.remove('rolagem');
     }
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    function imagemTransicao(bloco){
+        const imagem = bloco.querySelectorAll('img');
+        let transicaoAtual = 0;
+
+        function proximaImagem() {
+            imagem[transicaoAtual].classList.remove('ativo');
+            transicaoAtual = (transicaoAtual + 1) % imagem.length;
+            imagem[transicaoAtual].classList.add('ativo');
+        }
+
+        imagem[transicaoAtual].classList.add('ativo');
+        setInterval(proximaImagem, 3000);
+    }
+
+    const blocoDireito = document.querySelector('#idireita');
+    const blocoEsquerdo = document.querySelector('#iesquerda');
+
+    imagemTransicao(blocoDireito);
+    imagemTransicao(blocoEsquerdo);
+    
 });
